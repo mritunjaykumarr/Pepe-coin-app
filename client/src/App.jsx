7f3d9202f09e0d648870dc8a02447acc793ee382
DavidPiyush <81695381+DavidPiyush@users.noreply.github.com>
2024-08-16 20:58:37 +0530

    update the file more

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Navbar, Hero, Services, Transactions, Footer } from "./components";
 const App = () => {
   return (
     <div className="min-h-screen">
+<<<<<<< HEAD
       <Navbar />
       
       <Hero />
@@ -10,6 +11,14 @@ const App = () => {
       <Services />
       <Transactions />
       <Footer />
+=======
+      <div>
+        <Navbar />
+        <Hero />
+      </div>
+      {/* <Index /> */}
+      {/* <Services /> */}
+>>>>>>> d430efd (update the file more)
     </div>
   );
 };
