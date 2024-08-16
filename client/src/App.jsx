88f0c19d486d2d69cc988ec0fcec3ab11182c1ff
DavidPiyush <81695381+DavidPiyush@users.noreply.github.com>
2024-08-16 21:08:07 +0530

    error fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,22 +3,14 @@ import { Navbar, Hero, Services, Transactions, Footer } from "./components";
 const App = () => {
   return (
     <div className="min-h-screen">
-<<<<<<< HEAD
-      <Navbar />
-      
-      <Hero />
-
-      <Services />
-      <Transactions />
-      <Footer />
-=======
       <div>
         <Navbar />
         <Hero />
       </div>
-      {/* <Index /> */}
-      {/* <Services /> */}
->>>>>>> d430efd (update the file more)
+
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   );
 };
